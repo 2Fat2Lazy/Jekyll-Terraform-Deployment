38db01c4821a65ba53287237c5863384ea1204c6
2Fat2Lazy <jordanstreet@protonmail.com>
2021-08-11 17:49:51 +0100

    Merged Providers/Main.tf for clarity

--- Main.tf
+++ Main.tf
@@ -1,7 +1,21 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+terraform {
   backend "s3" {
     bucket = "<example bucket name>" #Name of the S3 bucket to store your Terraform state file
-    key    = "<example S3 key>" #For specifying directory structure of saved tfstate e.g tfstate/yourwebsite/tfstate.json
+    key    = "<example S3 key>"      #For specifying directory structure of saved tfstate e.g tfstate/yourwebsite/tfstate.json
     region = "eu-west-1"
   }
 }
